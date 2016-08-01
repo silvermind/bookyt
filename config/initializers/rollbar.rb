5b01ae7897337bab74092f1e1be450c9815ef28e
Severin Ulrich <sulrich@downloads.ch>
2016-08-01 12:37:25 +0200

    add hardcoded rb token

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -3,7 +3,7 @@ Rollbar.configure do |config|
   # Without configuration, Rollbar is enabled in all environments.
   # To disable in specific environments, set config.enabled=false.
   
-  config.access_token = ENV['ROLLBAR_ACCESS_TOKEN']
+  config.access_token = ENV['ROLLBAR_ACCESS_TOKEN'] || "09e489add11d449aa9c9271324b483c8"
 
   # Here we'll disable in 'test':
   if Rails.env.test? or Rails.env.development?
